73af3886abb4923de9f367a9d1de1c4c266746a4
Tugay Bayırlı <tgybyrl48@gmail.com>
2026-02-26 10:11:00 +0200

    Mobile navigation bar fixed.

--- js/script.js
+++ js/script.js
@@ -45,10 +45,15 @@ allLinks.forEach(function (link) {
         behavior: "smooth",
       });
 
-    if (href !== "#" && href.startsWith("#")) document.querySelector(href) {
-      const sectionEl = document.querySelector(href)
-      sectionEl.scrollIntoView({ behavior:"smooth"})
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
   });
 });
 
